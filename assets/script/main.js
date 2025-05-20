88373dca7ed8c6db80a8d7a77b00e4438d283059
Chinami0702 <chinamik0702@gmail.com>
2025-05-20 15:58:01 +0900

    八回目のコミット

--- assets/script/main.js
+++ assets/script/main.js
@@ -10,3 +10,10 @@ $(document).ready(function() {
     $(target).show();
   });
 });
+
+$(function () {
+  $('#js-hamburger-menu, .navigation__link').on('click', function () {
+    $('.nav_header').slideToggle(500)
+    $('.hamburger-menu').toggleClass('hamburger-menu--open')
+  });
+});
