ae8e18d1f07ea561d67c0f0d625a36ede8d25a7c
Chinami0702 <chinamik0702@gmail.com>
2025-07-22 16:04:28 +0900

    修正

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,18 +1,18 @@
 $(function () {
-  $('#js-hamburger-menu, .navigation__link').on('click', function () {
-    $('.navigation').slideToggle(500)
-    $('.hamburger-menu').toggleClass('hamburger-menu--open')
-  });
+    $('#js-hamburger-menu, .navigation__link').on('click', function () {
+        $('.navigation').slideToggle(500)
+        $('.hamburger-menu').toggleClass('hamburger-menu--open')
+    });
 });
-  
-$(function () {
-  $(".tab-btn").on("click", function () {
-    $(".tab-btn").removeClass("active");
-    $(this).addClass("active");
 
-    $(".main_cource-box").hide();
-    $($(this).data("target")).show();
-  });
+$(function () {
+    $(".tab-btn").on("click", function () {
+        $(".tab-btn").removeClass("active");
+        $(this).addClass("active");
+
+        $(".main_cource-box").hide();
+        $($(this).data("target")).show();
+    });
 });
 
 
